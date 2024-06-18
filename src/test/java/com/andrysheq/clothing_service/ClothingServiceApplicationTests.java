bd179525e9011e856aa872754e68af40e38bdfe3
kareevaa <kareev@i-teco.ru>
2024-06-18 12:05:36 +0300

    Initial commit

---
+++ src/test/java/com/andrysheq/clothing_service/ClothingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrysheq.clothing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
